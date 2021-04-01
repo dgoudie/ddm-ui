a03ae5d2d9a0628e87ac3d3ad744f4dbb42f5e85
dgoudie <34222208+dgoudie@users.noreply.github.com>
2021-04-01 00:16:31 -0500

    added dialog

--- src/views/beers-and-liquors/BeersAndLiquors.tsx
+++ src/views/beers-and-liquors/BeersAndLiquors.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { BeerOrLiquorBrand } from '@stan/ddm-types';
 import { LoggedInStatusContext } from '../../App';
@@ -8,6 +8,10 @@ import { useFetchFromApi } from '../../utils/fetch-from-api';
 
 export default function BeersAndLiquors() {
     const response = useFetchFromApi<BeerOrLiquorBrand[]>(`/beers-and-liquors`);
+    const [
+        selectedBeerOrLiquor,
+        setSelectedBeerOrLiquor,
+    ] = useState<BeerOrLiquorBrand | null>(null);
     if (!response?.data) {
         return null;
     }
@@ -18,16 +22,28 @@ export default function BeersAndLiquors() {
                     <BeerOrLiquor
                         key={beerOrLiquor._id}
                         beerOrLiquor={beerOrLiquor}
+                        onSelected={() => setSelectedBeerOrLiquor(beerOrLiquor)}
                     />
                 ))}
             </div>
+            {!!selectedBeerOrLiquor && (
+                <BeerOrLiquorActions
+                    beerOrLiquor={selectedBeerOrLiquor}
+                    onClosed={() => setSelectedBeerOrLiquor(null)}
+                />
+            )}
         </React.Fragment>
     );
 }
 
-function BeerOrLiquor({ beerOrLiquor }: { beerOrLiquor: BeerOrLiquorBrand }) {
+function BeerOrLiquor({
+    beerOrLiquor,
+    onSelected,
+}: {
+    beerOrLiquor: BeerOrLiquorBrand;
+    onSelected: () => void;
+}) {
     const { loggedIn } = useContext(LoggedInStatusContext);
-    // const [menuOpen, setMenuOpen] = useState(false);
     return (
         <div className={styles.listItem}>
             <div className={styles.listItemInfo}>
@@ -45,11 +61,75 @@ function BeerOrLiquor({ beerOrLiquor }: { beerOrLiquor: BeerOrLiquorBrand }) {
             </div>
             {loggedIn && (
                 <div className={styles.listItemActions}>
-                    <button>
+                    <button
+                        className={styles.listItemActionsButton}
+                        onClick={onSelected}
+                    >
                         <i className='fas fa-ellipsis-v' />
                     </button>
                 </div>
             )}
+        </div>
+    );
+}
+
+function BeerOrLiquorActions({
+    beerOrLiquor,
+    onClosed,
+}: {
+    beerOrLiquor: BeerOrLiquorBrand;
+    onClosed: () => void;
+}) {
+    const [confirmDelete, setConfirmDelete] = useState(false);
+    return (
+        <div className={styles.dialog}>
+            <div className={styles.dialogBackdrop} onClick={onClosed}></div>
+            <div className={styles.dialogBody}>
+                <header>
+                    <h3>{beerOrLiquor.name}</h3>
+                    <button onClick={onClosed}>
+                        <i className='fas fa-times' />
+                    </button>
+                </header>
+                {!confirmDelete ? (
+                    <div className={styles.dialogBodyActions}>
+                        {beerOrLiquor.inStock ? (
+                            <button>
+                                <i className='fas fa-times' />
+                                Mark as Out-of-Stock
+                            </button>
+                        ) : (
+                            <button>
+                                <i className='fas fa-check' />
+                                Mark as In-Stock
+                            </button>
+                        )}
+                        <button onClick={() => setConfirmDelete(true)}>
+                            <i className='fas fa-trash' />
+                            Delete
+                        </button>
+                    </div>
+                ) : (
+                    <div className={styles.dialogBodyConfirmDelete}>
+                        <span>
+                            Are you sure you'd like to delete{' '}
+                            {beerOrLiquor.name}? This will also delete any mixed
+                            drinks where {beerOrLiquor.name} is an ingredient.
+                        </span>
+                        <section>
+                            <button
+                                className={styles.dialogBodyConfirmDeleteYes}
+                                onClick={() => setConfirmDelete(true)}
+                            >
+                                Yes
+                            </button>
+                            <button onClick={() => setConfirmDelete(false)}>
+                                No
+                            </button>
+                        </section>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
